0ff06dce65a559bbcf30e3680568a52d99f6f80c
dlarte <dmolarte2@student.apc.edu.ph>
2023-02-27 15:58:37 +0800

    ci4 installation

---
+++ ci4/app/Views/pages/resource.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>APC WEBPROG website (olarte)</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href = "style/css.css">
+<style>
+a.ex2:hover, a.ex2:active {font-size: 125%;}
+a.ex3:hover, a.ex2:active {font-size: 110%;}
+</style>
+</head>
+
+
+<body style="background-color:#E2E8CE;cursor: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/9632/happy.png), auto">
+
+  
+
+  <!-- resource -->
+  <div class="w3-content w3-justify w3-padding-64" id="about">
+	
+    <p style="float:right;font-size:18px;cursor: help"> 
+	<a a class="ex3" href="http://apcwebprog.csf.ph/~dmolarte2/lab2/week5/MyProfile/index.html">Go back to home</a></p>
+	
+	
+	
+    <h2 style="font-color:#0F0F0F;font-size:40px" >Resources</h2>
+	
+	
+	
+    <hr style="height:2px;border-width:0;color:gray;background-color:#0F0F0F">
+	<h3 style="font-color:#0F0F0F;">HTML and CSS :</h3>
+    <p style="font-color:#363636" ><a a class="ex2" href="https://www.w3schools.com/html/default.asp" > - W3Schools HTML Website </a></p>
+	<p style="font-color:#363636"><a a class="ex2" href="https://www.w3schools.com/css/" >- W3Schools CSS Website</a></p>
+	<h3 style="font-color:#0F0F0F;">Web Stylesheet :</h3>
+    <p style="font-color:#363636"><a a class="ex2" href="https://fonts.googleapis.com/css?family=Montserrat" >- Googleapis</a></p>
+	<p style="font-color:#363636"><a a class="ex2" href="https://fontawesome.com/" >- FontAwesome</a></p>
+	<h3 style="font-color:#0F0F0F;">Github Repository :</h3>
+	<p style="font-color:#363636"><a a class="ex2" href="https://github.com/Dmolarte2/lab2/tree/main/week5/MyProfile" >- Github File Repository</a></p>
+	<h3 style="font-color:#0F0F0F;">Images :</h3>
+	<p style="font-color:#363636"><a a class="ex2" href="https://github.com/Dmolarte2/lab2/tree/main/week5/MyProfile/images" >- Images Repository</a></p>
+	<h3 style="font-color:#0F0F0F;">CSS :</h3>
+	<p style="font-color:#363636"><a a class="ex2" href="https://github.com/Dmolarte2/lab2/tree/main/week5/MyProfile/style" >- CSS Repository</a></p>
+    
+	
+    
+    
+
+  <!-- contact eme -->
+  <div class=" w3-content" id="contact">
+  
+    <h2 style="font-color:#0F0F0F;padding-top:100px">Contact Me</h2>
+	<hr style="height:2px;border-width:0;width:400px;color:gray;background-color:#0F0F0F;">
+    
+
+    <div>
+	
+		<p ><a href="https://www.facebook.com/daaaaaniellrt/"><i class="fa fa-facebook-official w3-hover-opacity w3-margin-right"></i> </a><a href="https://www.facebook.com/daaaaaniellrt/" style="font-color:#363636" >@daaaaaaniel.lrt</a></p>
+		<p><a href="https://www.instagram.com/daniel.lrt/?hl=en"><i class="fa fa-instagram w3-hover-opacity w3-margin-right"></i> </a> <a href="https://www.instagram.com/daniel.lrt/?hl=en">@daniel.lrt</a></p>
+		<p><a href="https://www.linkedin.com/in/daniel-olarte-a3290318b/"><i class="fa fa-linkedin w3-hover-opacity w3-margin-right"></i> </a> <a href="https://www.linkedin.com/in/daniel-olarte-a3290318b/"> Daniel Olarte</a></p>
+    
+    </div><br>
+    
+
+  </div>
+  
+  
+  
+    <!-- baba -->
+  <footer class="w3-content w3-padding-64 w3-xlarge">
+    <hr style="height:2px;border-width:0;color:gray;background-color:#0F0F0F">
+    <p class="w3-medium">Powered by <a  target="_blank" >Yours trulyw3</a></p>
+	
+  </footer>
+
+<!-- tapos na -->
+</div>
+
+</body>
+</html>
---
+++ ci4/app/Views/templates/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+
+    <title>Daniel Olarte's Website</title>
+</head>
+<body>
+
